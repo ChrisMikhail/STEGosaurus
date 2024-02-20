9f05c858519aa93028a0d9f223ffe1c3d8000348
Chris Mikhail <samuelmikhc@gmail.com>
2024-02-19 22:19:40 -0500

    Cleaned up layout and added more button functionalities

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 import customtkinter
+from tkinter import filedialog
+
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
@@ -13,8 +15,9 @@ def change_appearance_mode_event(new_appearance_mode: str):
     customtkinter.set_appearance_mode(new_appearance_mode)
 
 
-def sidebar_button_event():
+def openfile():
     print("sidebar_button click")
+    return filedialog.askopenfilename()
 
 
 def change_scaling_event(new_scaling: str):
@@ -27,6 +30,8 @@ class App(customtkinter.CTk):
         super().__init__()
 
         # configure window
+        self.main_button_1 = None
+        self.entry = None
         self.title("STEGosaurus - LSB Image Steganography Tool")
         self.geometry(f"{1100}x{580}")
 
@@ -35,6 +40,18 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
+        def encode_clicked():
+            self.entry = customtkinter.CTkEntry(self, placeholder_text="Message to be encoded")
+            self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+
+            self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2,
+                                                         text_color=("gray10", "#DCE4EE"), text="Submit")
+            self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+        def remove_elements():
+            self.entry.destroy()
+            self.main_button_1.destroy()
+
         # create sidebar frame with widgets
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
@@ -42,19 +59,19 @@ class App(customtkinter.CTk):
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="STEGosaurus",
                                                  font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, command=sidebar_button_event,
-                                                        text="Encode")
+        self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame,
+                                                        text="Home", command=remove_elements)
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=sidebar_button_event,
-                                                        text="Decode")
+        self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=encode_clicked,
+                                                        text="Encode")
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=sidebar_button_event,
-                                                        text="Image Upload")
+        self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=openfile,
+                                                        text="Decode")
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
-                                                                       values=["Light", "Dark", "System"],
+                                                                       values=["System", "Dark", "Light"],
                                                                        command=change_appearance_mode_event)
         self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
         self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w")
@@ -63,18 +80,6 @@ class App(customtkinter.CTk):
                                                                values=["80%", "90%", "100%", "110%", "120%"],
                                                                command=change_scaling_event)
         self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
-
-        # create main entry and button
-        self.entry = customtkinter.CTkEntry(self, placeholder_text="Message to be encoded")
-        self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
-
-        self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2,
-                                                     text_color=("gray10", "#DCE4EE"), text="Submit")
-        self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
-
-        # create textbox
-        self.textbox = customtkinter.CTkTextbox(self, width=250)
-        self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
 
 
 if __name__ == "__main__":
