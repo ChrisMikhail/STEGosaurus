d87d0af82d32e75320d4ba7ec3eac4298daf05f8
Chris Mikhail <samuelmikhc@gmail.com>
2024-02-21 14:16:41 -0500

    read_yaml_file() now reads specific info vs loading all info

--- open_yaml.py
+++ open_yaml.py
@@ -3,5 +3,10 @@ import yaml
 
 def read_yaml_file(file_path="secrets.yaml"):
     with open(file_path, 'r') as file:
-        data = yaml.safe_load(file)
-    return data
+        yaml_data = yaml.safe_load(file)
+        secrets = yaml_data["secrets"]
+        key = eval(secrets["key"].encode('utf-8'))
+        nonce = eval(secrets["nonce"].encode('utf-8'))
+        tag = eval(secrets["tag"].encode('utf-8'))
+        length = int(secrets["length"])
+    return key, nonce, tag, length
