d6b92b06a5f6a6e9edf224e72ea78fd9f942acc2
Chris Mikhail <samuelmikhc@gmail.com>
2024-02-23 00:52:28 -0500

    Made Encode page format consistent with Intro

--- gui.py
+++ gui.py
@@ -22,26 +22,6 @@ def openfile():
 
 class App(customtkinter.CTk):
     def __init__(self):
-        super().__init__()
-
-        # configure window
-        self.message_label = None
-        self.main_button_1 = None
-        self.entry = None
-        self.title("STEGosaurus - LSB Image Steganography Tool")
-        self.geometry(f"{1100}x{580}")
-
-        # Loads images
-        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "static")
-        self.first_image_path = os.path.join(image_path, "steg.png")
-        self.second_image_path = os.path.join(image_path, "sus_steg.png")
-        self.first_test_image = customtkinter.CTkImage(Image.open(self.first_image_path), size=(402, 301))
-        self.second_test_image = customtkinter.CTkImage(Image.open(self.second_image_path), size=(402, 301))
-
-        # configure grid layout
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure((2, 3), weight=0)
-        self.grid_rowconfigure((0, 1, 2), weight=1)
 
         def update_message_label():
             """Updates label text"""
@@ -51,94 +31,132 @@ class App(customtkinter.CTk):
 
         def encode_clicked():
             """Adds extra UI components for the encode section"""
-            self.entry = customtkinter.CTkEntry(self, placeholder_text="Message to be encoded")
-            self.entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+            if self.title_label.cget("text") != "Encode":
+                self.title_label.configure(text="Encode")
+                self.bottom_frame.grid_forget()
+                self.content_frame.grid_forget()
+                self.entry = customtkinter.CTkEntry(self, placeholder_text="Message to be encoded")
+                self.entry.grid(row=2, column=1, padx=(15, 15), sticky="nsew")
 
-            self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2,
-                                                         text_color=("gray10", "#DCE4EE"), text="Submit")
-            self.main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+                # Submission button
+                self.main_button_1 = customtkinter.CTkButton(master=self, fg_color="transparent", border_width=2,
+                                                             text_color=("gray10", "#DCE4EE"), text="Submit")
+                self.main_button_1.grid(row=3, column=1, padx=(15, 15), pady=(5, 15), sticky="nsew")
+                self.columnconfigure(1, weight=1)
+                self.rowconfigure(2, weight=1)
+                self.rowconfigure(3, weight=1)
+            else:
+                pass
 
-        def remove_elements():
+        def intro_clicked():
             """Removes all extra components for the home page"""
-            # Create the top frame
-            self.top_frame = customtkinter.CTkFrame(self, fg_color="transparent")
-            self.top_frame.grid(row=0, column=1, padx=20, sticky="ew")
-            self.grid_rowconfigure(0, weight=10)  # Top frame
-
-            # Create the label in the top frame
-            introduction_label = customtkinter.CTkLabel(self.top_frame, text="Introduction",
-                                                        font=customtkinter.CTkFont(size=20, weight="bold"))
-            introduction_label.pack(side="top", fill="both", expand=True)
-
-            # Create the middle content frame
-            self.content_frame = customtkinter.CTkFrame(self)
-            self.content_frame.grid(row=1, column=1, padx=20, sticky="nsew")
-            self.grid_rowconfigure(1, weight=150)  # Middle content frame
-
-            # Create the first image label with title "Original"
-            self.home_frame_first_image_label = customtkinter.CTkLabel(self.content_frame, text="",
-                                                                       image=self.first_test_image)
-            self.home_frame_first_image_label.grid(row=2, column=0, padx=20, pady=10)
-            self.home_frame_first_image_label.place(x=15, y=100)
-
-            # Create the title label for the first image
-            original_title_label = customtkinter.CTkLabel(self.content_frame, text="Original Image",
-                                                          font=customtkinter.CTkFont(size=16, weight="bold"))
-            original_title_label.grid(row=2, column=0, padx=20, pady=5)
-            original_title_label.place(x=15, y=70)
-
-            # Create the second image label with title "Secret"
-            self.home_frame_second_image_label = customtkinter.CTkLabel(self.content_frame, text="",
-                                                                        image=self.second_test_image)
-            self.home_frame_second_image_label.grid(row=2, column=0, padx=20, pady=10)
-            self.home_frame_second_image_label.place(x=465, y=100)
-
-            # Create the title label for the second image
-            secret_title_label = customtkinter.CTkLabel(self.content_frame, text="Contains a Hidden Message",
-                                                        font=customtkinter.CTkFont(size=16, weight="bold"))
-            secret_title_label.grid(row=2, column=0, padx=20, pady=5)
-            secret_title_label.place(x=465, y=70)
-
-            # Create the "Decode" button in the middle content frame
-            decode_button = customtkinter.CTkButton(self.content_frame, text="Decode",
-                                                    font=customtkinter.CTkFont(size=14), command=update_message_label)
-            decode_button.pack(side="bottom", pady=15)
-
-            # Create the bottom frame
-            self.bottom_frame = customtkinter.CTkFrame(self, fg_color="transparent")
-            self.bottom_frame.grid(row=2, column=1, padx=20, sticky="ew")
-            self.grid_rowconfigure(2, weight=10)  # Bottom frame
-
-            # Create the label in the bottom frame with left alignment
-            self.message_label = customtkinter.CTkLabel(self.bottom_frame,
-                                                        text="",
-                                                        font=customtkinter.CTkFont(size=16, weight="bold"))
-            self.message_label.pack(side="left", fill="both")
-
             try:
+
+                self.title_label.configure(text="Introduction")
+                self.bottom_frame.grid(row=2, column=1, padx=20, sticky="ew")
+                self.content_frame.grid(row=1, column=1, padx=20, sticky="nsew")
+                self.grid_rowconfigure(2, weight=10)
+
                 self.entry.destroy()
                 self.main_button_1.destroy()
             except AttributeError:
                 pass
 
-        remove_elements()
+        super().__init__()
 
-        # create sidebar frame with widgets
+        # Configure grid
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+
+        # Title frame
+        self.title_frame = customtkinter.CTkFrame(self, fg_color="transparent")
+        self.title_frame.grid(row=0, column=1, padx=20, sticky="ew")
+        self.grid_rowconfigure(0, weight=10)
+        self.title_label = customtkinter.CTkLabel(self.title_frame, text="Introduction",
+                                                  font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.title_label.pack(side="top", fill="both", expand=True)
+
+        # Loads images
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "static")
+        self.first_image_path = os.path.join(image_path, "steg.png")
+        self.second_image_path = os.path.join(image_path, "sus_steg.png")
+        self.first_test_image = customtkinter.CTkImage(Image.open(self.first_image_path), size=(402, 301))
+        self.second_test_image = customtkinter.CTkImage(Image.open(self.second_image_path), size=(402, 301))
+
+        # Middle Frame
+        self.content_frame = customtkinter.CTkFrame(self)
+        self.content_frame.grid(row=1, column=1, padx=20, sticky="nsew")
+        self.grid_rowconfigure(1, weight=150)  # Middle content frame
+
+        # Original image
+        self.home_frame_first_image_label = customtkinter.CTkLabel(self.content_frame, text="",
+                                                                   image=self.first_test_image)
+        self.home_frame_first_image_label.grid(row=2, column=0, padx=20, pady=10)
+        self.home_frame_first_image_label.place(x=15, y=100)
+
+        # Original image label
+        original_title_label = customtkinter.CTkLabel(self.content_frame, text="Original Image",
+                                                      font=customtkinter.CTkFont(size=16, weight="bold"))
+        original_title_label.grid(row=2, column=0, padx=20, pady=5)
+        original_title_label.place(x=15, y=70)
+
+        # Second image
+        self.home_frame_second_image_label = customtkinter.CTkLabel(self.content_frame, text="",
+                                                                    image=self.second_test_image)
+        self.home_frame_second_image_label.grid(row=2, column=0, padx=20, pady=10)
+        self.home_frame_second_image_label.place(x=465, y=100)
+
+        # Second image label
+        secret_title_label = customtkinter.CTkLabel(self.content_frame, text="Contains a Hidden Message",
+                                                    font=customtkinter.CTkFont(size=16, weight="bold"))
+        secret_title_label.grid(row=2, column=0, padx=20, pady=5)
+        secret_title_label.place(x=465, y=70)
+
+        # Decode button
+        decode_button = customtkinter.CTkButton(self.content_frame, text="Decode",
+                                                font=customtkinter.CTkFont(size=14), command=update_message_label)
+        decode_button.pack(side="bottom", pady=15)
+
+        # Bottom frame
+        self.bottom_frame = customtkinter.CTkFrame(self, fg_color="transparent")
+        self.bottom_frame.grid(row=2, column=1, padx=20, sticky="ew")
+        self.grid_rowconfigure(2, weight=10)
+
+        # Displays message
+        self.message_label = customtkinter.CTkLabel(self.bottom_frame,
+                                                    text="",
+                                                    font=customtkinter.CTkFont(size=16, weight="bold"))
+        self.message_label.pack(side="left", fill="both")
+
+        # Basic configuration
+        self.title("STEGosaurus - LSB Image Steganography Tool")
+        self.geometry(f"{1100}x{580}")
+
+        # sidebar
         self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="STEGosaurus",
                                                  font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+
+        # Intro button
         self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame,
-                                                        text="Introduction", command=remove_elements)
+                                                        text="Introduction", command=intro_clicked)
         self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+
+        # Encode button
         self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, command=encode_clicked,
                                                         text="Encode")
         self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+
+        # Decode button
         self.sidebar_button_3 = customtkinter.CTkButton(self.sidebar_frame, command=openfile,
                                                         text="Decode")
         self.sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
+
+        # Appearance dropdown
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
         self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
